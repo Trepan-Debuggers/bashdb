045a0aa235776b66d3254a272125ea3942f74cfa
Joachim Ansorg <github@ja-dev.eu>
2024-02-14 10:59:11 +0100

    Don't compare unstable highlighting of Pygments, but only the file locations

--- test/example/bug-loc.sh
+++ test/example/bug-loc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Bug is that file cache highlight isn't updated when when
+# Bug is that file cache highlight isn't updated when
 # it is already cached so it continues to refer to a line in the source'd file
 # rather than file it was source'd from (here it is this file).
 dirname=${BASH_SOURCE%/*}   # equivalent to dirname($0)
