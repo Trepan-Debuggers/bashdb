d1d256f4d27d23eb98e23bb2d3d085cfe5832415
Joachim Ansorg <github@ja-dev.eu>
2024-06-18 17:51:10 +0200

    add sort_list_named to sort an array variable in-place via nameref. (#27)
    This avoids the requirement of a global "list" variable.

--- lib/sort.sh
+++ lib/sort.sh
@@ -8,30 +8,42 @@
 sort_list() {
   (($# != 2)) && return 1
   typeset -i left=$1
-  ((left < 0)) || (( 0 == ${#list[@]})) && return 2
+  ((left < 0)) || ((0 == ${#list[@]})) && return 2
   typeset -i right=$2
   ((right >= ${#list[@]})) && return 3
-  typeset -i i=$left; typeset -i j=$right
-  typeset -i mid; ((mid= (left+right) / 2))
-  typeset partition_item; partition_item="${list[$mid]}"
+  typeset -i i=$left
+  typeset -i j=$right
+  typeset -i mid
+  ((mid = (left + right) / 2))
+  typeset partition_item
+  partition_item="${list[$mid]}"
   typeset temp
-  while ((j > i)) ; do
-      item=${list[i]}
-      while [[ "${list[$i]}" < "$partition_item" ]] ; do
-	  ((i++))
-      done
-      while [[ "${list[$j]}" > "$partition_item" ]] ; do
-	  ((j--))
-      done
-      if ((i <= j)) ; then
-	  temp="${list[$i]}"; list[$i]="${list[$j]}"; list[$j]="$temp"
-          ((i++))
-          ((j--))
-      fi
+  while ((j > i)); do
+    item=${list[i]}
+    while [[ "${list[$i]}" < "$partition_item" ]]; do
+      ((i++))
+    done
+    while [[ "${list[$j]}" > "$partition_item" ]]; do
+      ((j--))
+    done
+    if ((i <= j)); then
+      temp="${list[$i]}"
+      list[$i]="${list[$j]}"
+      list[$j]="$temp"
+      ((i++))
+      ((j--))
+    fi
   done
-  ((left < j))  && sort_list $left  $j  
+  ((left < j)) && sort_list $left $j
   ((right > i)) && sort_list $i $right
   return 0
+}
+
+# Sorts the array variable passed as 1st parameter from index $2 to index $3.
+sort_list_named() {
+  (($# != 3)) && return 1
+  typeset -n list="$1"
+  sort_list "$2" "$3"
 }
 
 if [[ $0 == *sorting.sh ]] ; then 
