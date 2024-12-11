f93e14ba3f0e3c263a85cb8a09f3a8730f22629e
R. Bernstein <rocky@users.noreply.github.com>
2024-12-11 06:44:18 -0500

    Resolve filenames in `backtrace` command (#54)
    * Resolve file paths in "backtrace"
    * Save mapping when doing the "load" command; resolve the filename in  the "backtrace" command
    * Add a test for the "load" command
    
    ---------
    
    Co-authored-by: Joachim Ansorg <github@ja-dev.eu>

--- command/load.sh
+++ command/load.sh
@@ -2,7 +2,7 @@
 # Debugger load SCRIPT command.
 #
 #   Copyright (C) 2002-2006, 2008, 2010-2011, 2018-2019 Rocky
-#   Bernstein <rocky@gnu.org>
+#   2024 Bernstein <rocky@gnu.org>
 #
 #   This program is free software; you can redistribute it and/or
 #   modify it under the terms of the GNU General Public License as
@@ -54,6 +54,8 @@ _Dbg_do_load() {
 
     _Dbg_readin "$_Dbg_full_filename"
     _Dbg_msg "File $_Dbg_full_filename loaded."
+    _Dbg_file2canonic["${_Dbg_filename}"]="$_Dbg_full_filename"
+
   else
       _Dbg_errmsg "Couldn't resolve or read $_Dbg_filename"
       return 3
