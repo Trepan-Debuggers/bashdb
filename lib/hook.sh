3306f5b7f3671e88e8dc15dc1e94cbb1efec0a98
rocky <rb@dustyfeet.com>
2024-12-15 15:15:01 -0500

    Save more file information from initial call...
    In particular, resolve the initial source directory and store that as
    _Dbg_init_dir. Resolve the name of main bash program and add that to
    canonicalized filenames.

--- lib/hook.sh
+++ lib/hook.sh
@@ -119,6 +119,13 @@ _Dbg_debug_trap_handler() {
 
     _Dbg_save_args "$@"
 
+    typeset _Dbg_full_filename
+    _Dbg_full_filename=$(_Dbg_is_file "${BASH_SOURCE[1]}")
+    if [[ -r "$_Dbg_full_filename" ]] ; then
+	_Dbg_file2canonic["${BASH_SOURCE[1]}"]="$_Dbg_full_filename"
+    fi
+
+
     # if in step mode, decrement counter
     if ((_Dbg_step_ignore > 0)) ; then
 	((_Dbg_step_ignore--))
@@ -145,13 +152,6 @@ _Dbg_debug_trap_handler() {
 	    fi
 	fi
     done
-
-    typeset _Dbg_full_filename
-    _Dbg_full_filename=$(_Dbg_is_file "$_Dbg_frame_last_filename")
-    if [[ -r "$_Dbg_full_filename" ]] ; then
-	_Dbg_file2canonic["$_Dbg_frame_last_filename"]="$_Dbg_full_filename"
-	_Dbg_file2canonic["${BASH_SOURCE[1]}"]="$_Dbg_full_filename"
-    fi
 
     # Run applicable action statement
     if ((_Dbg_action_count > 0)) ; then
