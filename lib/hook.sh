4ec78ebb2b665457a971fb6e623368aa9b809d5c
R. Bernstein <rocky@users.noreply.github.com>
2024-12-17 05:50:50 -0500

    Update lib/hook.sh
    Co-authored-by: Joachim Ansorg <github@ja-dev.eu>

--- lib/hook.sh
+++ lib/hook.sh
@@ -120,7 +120,7 @@ _Dbg_debug_trap_handler() {
     _Dbg_save_args "$@"
 
     typeset _Dbg_full_filename
-    _Dbg_full_filename=$(_Dbg_is_file "${BASH_SOURCE[1]}")
+    _Dbg_full_filename="$(_Dbg_is_file "${BASH_SOURCE[1]}")"
     if [[ -r "$_Dbg_full_filename" ]] ; then
 	_Dbg_file2canonic["${BASH_SOURCE[1]}"]="$_Dbg_full_filename"
     fi
