01576801abafae430ace44f2968ba682dfca8b57
rocky <rb@dustyfeet.com>
2024-12-23 06:19:08 -0500

    Add more places we resolve/expand files
    In some cases though automatic file loading no longer happens.
    In particular, on "list" and "break" commands.

--- command/load.sh
+++ command/load.sh
@@ -52,7 +52,11 @@ _Dbg_do_load() {
     done
 
     _Dbg_readin "$_Dbg_full_filename"
-    _Dbg_msg "File $_Dbg_full_filename loaded."
+    if (( _Dbg_set_basename )) ; then
+	_Dbg_msg "File $_Dbg_filename loaded."
+    else
+	_Dbg_msg "File $_Dbg_full_filename loaded."
+    fi
     _Dbg_file2canonic["${_Dbg_filename}"]="$_Dbg_full_filename"
 
   else
--- test/data/list.cmd
+++ test/data/list.cmd
@@ -2,20 +2,21 @@ set trace-commands on
 # Test of debugger 'list' command
 #
 ### List default location
-list 
+list
 ### Should list next set of lines
 l
 #
 # Should not see anything since we ran off the top
-# 
+#
 list 999
 #########################################################
 ### 'list file:line' and canonicalization of filenames...
+load ../example//dbg-test1.sh
 list ../example//dbg-test1.sh:1
 list ../example/dbg-test1.sh:20
 list ../example/dbg-test1.sh:30
 list ../example//dbg-test1.sh:999
-list ./badfile:1
+load ./badfile
 #########################################################
 set trace-commands on
 ### list of functions...
@@ -23,8 +24,8 @@ list fn1
 list bogus
 #########################################################
 ###  Testing '.'
-l . 
-# 
+l .
+#
 # Should see lines up to current execution line.
 ###  Trying '-'...
 l -
